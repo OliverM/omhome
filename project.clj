188fb422dc2551d340e0346c42dc9582196889c4
Oliver Mooney <Oliver.Mooney@gmail.com>
2014-09-29 00:46:32 +0100

    Fragments now working (or 'partials' if you must...)

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [stasis "2.2.2"]
-                 [ring "1.3.1"]]
+                 [ring "1.3.1"]
+                 [hiccup "1.0.5"]]
   :ring {:handler omhome.ssgen/app}
   :main ^:skip-aot omhome.core
   :target-path "target/%s"
--- src/omhome/ssgen.clj
+++ src/omhome/ssgen.clj
@@ -1,8 +1,25 @@
 (ns omhome.ssgen
-  (:require [stasis.core :as stasis]))
+  (:require [stasis.core :as stasis]
+            [hiccup.page :refer [html5]]
+            [clojure.java.io :as io]))
+
+(defn layout-page [page]
+  (html5
+    [:head
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport"
+             :content "width=device-width, initial-scale 1.0"}]
+     [:title "Everything is Placeholder"]
+     [:link {:rel "stylesheet" :href "/css/pages.css"}]]
+    [:body
+     [:div.logo "olivermooney.com"]
+     [:div.body page]]))
+
+(defn about-page [request]
+  (layout-page (slurp (io/resource "fragments/about.html"))))
 
 (defn get-pages []
-  (stasis/slurp-directory "resources/pages" #".*\.(html|css|js)"))
+  (merge (stasis/slurp-directory "resources/pages" #".*\.(html|css|js)")
+         {"/about/" about-page}))
 
 (def app (stasis/serve-pages get-pages))
-
