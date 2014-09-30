c8c2250ae1ac1c2f0157066300a8a42907e5ea37
Oliver Mooney <Oliver.Mooney@gmail.com>
2014-09-30 15:53:50 +0100

    Markdown integration. I've shied away from using Markdown in the past as it's yet another templating library that doesn't seem to support what I'd like to be able to do (attach arbitrary CSS classes and ids to elements that I can catch in my styling) but perhaps I need to either stop styling things that way or check what the Cegdown (wrapping Pegdown) extensions offer

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [stasis "2.2.2"]
                  [ring "1.3.1"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [me.raynes/cegdown "0.1.1"]]
   :ring {:handler omhome.ssgen/app}
   :main ^:skip-aot omhome.core
   :target-path "target/%s"
--- src/omhome/ssgen.clj
+++ src/omhome/ssgen.clj
@@ -1,7 +1,9 @@
 (ns omhome.ssgen
   (:require [stasis.core :as stasis]
             [hiccup.page :refer [html5]]
-            [clojure.java.io :as io]))
+            [me.raynes.cegdown :as md]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (defn layout-page [page]
   (html5
@@ -19,10 +21,16 @@
   (zipmap (keys pages)
           (map layout-page (vals pages))))
 
+;; rework the stasis map of filenames to source content created using markdown to use paths and html
+(defn markdown->pages [markdown-content]
+  (zipmap (map #(str/replace % #"\.md$" "/") (keys markdown-content)) ;; convert paths ending in .md to paths ending in /
+          (map #(layout-page (md/to-html %)) (vals markdown-content)))) ;; convert markdown to html
+
 (defn get-pages []
   (stasis/merge-page-sources
     {:pages (stasis/slurp-directory "resources/pages" #".*\.(html|css|js$)")
-     :templated-pages (content->pages (stasis/slurp-directory "resources/fragments" #".*\.html$"))}))
+     :templated-pages (content->pages (stasis/slurp-directory "resources/fragments" #".*\.html$"))
+     :markdown-pages (markdown->pages (stasis/slurp-directory "resources/markdown" #".*\.md$"))}))
 
 (def app (stasis/serve-pages get-pages))
 
