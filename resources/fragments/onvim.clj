347a95506265739c8be3fc6ea12617a51f4f9119
Oliver Mooney <oliver.mooney@gmail.com>
2016-04-13 10:08:20 +0100

    Drafting an article on my experience using VIM

---
+++ resources/fragments/onvim.clj
@@ -0,0 +1,15 @@
+[:article
+ [:h1 "Complaints about VIM as I Struggle to Master It"]
+ [:h2 "~ or ~"]
+ [:h1 "Straw Horses & VIM: How They Tell You Editing Sucks, VS How It Actually Sucks"]
+ [:h2 "Oliver Mooney"]
+ [:p [:span.newthought "I am typing this in "
+      [:a {:href "http://spacemacs.org"} "Spacemacs"]
+      ]
+  ", a version of Emacs that's been overhauled to operate like Vim."]
+
+ [:ul
+  [:li "The mouse is BAD (not if you've played video games all your life, it's not; first-person shooters are ALL ABOUT hitting arbitrary tiny targets fast, and I've been doing that for a decade or more for fun; and these days the touchpad on laptops is always in the same place, vastly reducing time needed to find & use it)"]
+  [:li "The cursor keys are BAD because... reasons?"]
+  [:li "Macros! Macros are the best. (Story about editing 23MB file here, long line lengths, v-a-t, automating in a macro)"]]]
+
