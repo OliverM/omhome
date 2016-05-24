06e08b25209facfcaf6300ba662d12521275bf73
Oliver Mooney <oliver.mooney@gmail.com>
2016-05-24 13:57:32 +0100

    Moved smartypantsjs regexps local to impl fn

--- src/omhome/transformations.clj
+++ src/omhome/transformations.clj
@@ -31,36 +31,37 @@
 (defn- includes? [s] (enlive/text-pred #(s/includes? % s)))
 (defn- re= [s] (enlive/text-pred #(boolean (re-find s %))))
 
-(def ellipsis "...")
-(def triple-prime "'''")
-(def double-prime "''")
-(def em-dash "---")
-(def en-dash "--")
-(def opening-double-quote #"(\W|^)\"(\S)")
-(def closing-double-quote #"(\u201c[^\"]*)\"([^\"]*$|[^\u201c\"]*\u201c)")
-(def closing-double-quote-supp #"([^0-9])\"")
-(def opening-apostrophe #"(\W|^)'(\S)")
-(def possessive-apostrophe #"i?([a-z])'([a-z])")
-(def closing-apostrophe #"i?((\u2018[^']*)|[a-z])'([^0-9]|$)")
-(def backwards-apostrophe #"i?(\B|^)\u2018(?=([^\u2019]*\u2019\b)*([^\u2019\u2018]*\W[\u2019\u2018]\b|[^\u2019\u2018]*$))")
-(def shortened-years-apostrophe #"i?(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])")
-(def standalone-apostrophe "'")
+
 
 (defn smartypants [html]
-  "Convert straight quotes to curly, whether single or double, and your sets of three periods to ellipses, and, verily, your multiple dashes to their rightful selves."
-  (enlive/sniptest html
-                   [(includes? ellipsis)] #(s/replace % ellipsis "\u2026")
-                   [(includes? triple-prime)] #(s/replace % triple-prime "\u2034")
-                   [(includes? double-prime)] #(s/replace % double-prime "\u2033")
-                   [(includes? em-dash)] #(s/replace % em-dash "\u2014")
-                   [(includes? en-dash)] #(s/replace % en-dash "\u2013")
-                   [(re= opening-double-quote)] #(s/replace % opening-double-quote "$1\u201c$2")
-                   [(re= closing-double-quote)] #(s/replace % closing-double-quote "$1\u201d$2")
-                   [(re= closing-double-quote-supp)] #(s/replace % closing-double-quote-supp "$1\u201d")
-                   [(re= opening-apostrophe)] #(s/replace % opening-apostrophe "$1\u2018$2")
-                   [(re= possessive-apostrophe)] #(s/replace % possessive-apostrophe "$1\u2019$2")
-                   [(re= closing-apostrophe)] #(s/replace % closing-apostrophe "$1\u2019$3")
-                   [(re= shortened-years-apostrophe)] #(s/replace % shortened-years-apostrophe "\u2019$2$3")
-                   [(re= backwards-apostrophe)] #(s/replace % backwards-apostrophe "$1\u2019")
-                   [(includes? standalone-apostrophe)] #(s/replace % standalone-apostrophe "\u2032")
-                   ))
+  "Convert straight quotes to curly, whether single or double, and your sets of three periods to ellipses, and, verily, your multiple dashes to their rightful selves. Regular expressions derived from smartquotesjs.com via https://github.com/kellym/smartquotesjs/blob/master/src/smartquotes.js"
+  (let [ellipsis "..."
+        triple-prime "'''"
+        double-prime "''"
+        em-dash "---"
+        en-dash "--"
+        opening-double-quote #"(\W|^)\"(\S)"
+        closing-double-quote #"(\u201c[^\"]*)\"([^\"]*$|[^\u201c\"]*\u201c)"
+        closing-double-quote-supp #"([^0-9])\""
+        opening-apostrophe #"(\W|^)'(\S)"
+        possessive-apostrophe #"i?([a-z])'([a-z])"
+        closing-apostrophe #"i?((\u2018[^']*)|[a-z])'([^0-9]|$)"
+        backwards-apostrophe #"i?(\B|^)\u2018(?=([^\u2019]*\u2019\b)*([^\u2019\u2018]*\W[\u2019\u2018]\b|[^\u2019\u2018]*$))"
+        shortened-years-apostrophe #"i?(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])"
+        standalone-apostrophe "'"]
+    (enlive/sniptest html
+                     [(includes? ellipsis)] #(s/replace % ellipsis "\u2026")
+                     [(includes? triple-prime)] #(s/replace % triple-prime "\u2034")
+                     [(includes? double-prime)] #(s/replace % double-prime "\u2033")
+                     [(includes? em-dash)] #(s/replace % em-dash "\u2014")
+                     [(includes? en-dash)] #(s/replace % en-dash "\u2013")
+                     [(re= opening-double-quote)] #(s/replace % opening-double-quote "$1\u201c$2")
+                     [(re= closing-double-quote)] #(s/replace % closing-double-quote "$1\u201d$2")
+                     [(re= closing-double-quote-supp)] #(s/replace % closing-double-quote-supp "$1\u201d")
+                     [(re= opening-apostrophe)] #(s/replace % opening-apostrophe "$1\u2018$2")
+                     [(re= possessive-apostrophe)] #(s/replace % possessive-apostrophe "$1\u2019$2")
+                     [(re= closing-apostrophe)] #(s/replace % closing-apostrophe "$1\u2019$3")
+                     [(re= shortened-years-apostrophe)] #(s/replace % shortened-years-apostrophe "\u2019$2$3")
+                     [(re= backwards-apostrophe)] #(s/replace % backwards-apostrophe "$1\u2019")
+                     [(includes? standalone-apostrophe)] #(s/replace % standalone-apostrophe "\u2032")
+                     )))
