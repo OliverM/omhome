c693dc4cfadda7a81b56fa61198a5dac8beab89e
Oliver Mooney <oliver.mooney@gmail.com>
2016-05-25 12:52:03 +0100

    Tag-set added
    Ultimately, tags entered in the :tags field of a post map should be
    present in the tagset too, or an exception will be raised. This should
    help catch typos in tags and in having an authorative set of tags

--- src/omhome/posts_meta.clj
+++ src/omhome/posts_meta.clj
@@ -3,20 +3,22 @@
 (def empty-post
   {:title ""
    :post-url "" ;; a custom url for the post
-   :post-filepath "" ;; a path to the post data. The filename suffix determines the post format (.md, .html or .clj for hiccup). 
+   :post-filepath "" ;; a path to the post data. The filename suffix determines the post format (.md, .html or .clj for hiccup).
    :publish false ;; set to true to include the post in the generated output, indices etc
    :date ""
    :category ""
    :tags []
-   :author "Oliver Mooney" 
+   :author "Oliver Mooney"
    })
+
+(def tagset #{}) ;; any tags cited in meta post data but not found here will raise an error (to help catch tag typos)
 
 (def posts
   [
    (assoc empty-post
           :title "Test meta hiccup post"
           :post-url "/hicplay/"
-          :post-filepath "resources/fragments/hicplay.clj") 
+          :post-filepath "resources/fragments/hicplay.clj")
    (assoc empty-post
           :title "On VIM"
           :post-url "/onvim/"
