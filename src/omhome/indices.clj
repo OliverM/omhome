1e9e37678a9d3b812a014681d0b4ab02ba903372
Oliver Mooney <oliver.mooney@gmail.com>
2016-05-27 12:33:06 +0100

    Improved index generation marginally
    - now generating a per-post entry but not resolved lazy fn properly

--- src/omhome/indices.clj
+++ src/omhome/indices.clj
@@ -1,6 +1,6 @@
 (ns omhome.indices
   (:require [net.cgrand.enlive-html :as html]
-            [omhome.posts-meta :as posts]))
+            [omhome.posts-meta :refer [  posts]]))
 
 (defn enlive->html
   "Convert the enlive-transformed templates into html using a Java StringBuilder"
@@ -11,7 +11,11 @@
     (.toString buf)))
 
 (html/deftemplate topic-index* "fragments/index-template.html" [tag]
-  [:section :> :p] (html/content "Introductory content for index placed by index generator."))
+  [:section :> :p] (html/content "Introductory content for index placed by index generator.")
+  [:article] (html/clone-for [post posts]
+                             [:h2] (html/html-content
+                                     (html/html [:a {:href (:post-url post)} (:title post) ])))
+  )
 
 (defn topic-index
   "Generate a html index of pages for the supplied tag, or complete page index if not tags are supplied."
