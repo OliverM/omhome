da7794002ba28524d90d914765a597726f34e951
Oliver Mooney <oliver.mooney@gmail.com>
2016-05-28 14:29:10 +0100

    Paper v. computer-mediated editing post draft

---
+++ resources/fragments/are-editors-good.clj
@@ -0,0 +1,9 @@
+[:article
+ [:h1 "Is using a blog as a technical journal even a good idea?"]
+ [:h2 "Current web editors offer only a tiny fraction of the freedom of ink & paper"]
+ [:p [:span.newthought "As I type, pages from Leonardo Da Vinci's Notebooks "] [:a {:href "http://www.nationalgallery.ie/en/aboutus/pressroom/2015_Press_Releases/Leonardo.aspx"} "are on display in the National Gallery."] " If you get the chance, they're worth seeing, and they're all easily viewable in under an hour."]
+ [:p "These are technical notes, not works of art. They're sketches and sentences assembled together to help develop projects, both commercial and creative."]
+ [:p "Da Vinci freely mixed text and illustration, adding new thoughts and additions where and when they best fit, given the constraints of the page. The page itself formed the frame for his thoughts, an unchangeable limit that resolved a ton of decisions todays editors force you to make: what typeface? What colour? And if you're the type to build your own layout, where does everything go, and how should that change from phone to laptop to desktop?"]
+ [:p "Maybe different times have different contraints; paper was precious in da Vinci's time, laboriously hand made from cloth rags, not pulverised wood fibres. You erased & reworked and patched as you went, & became adept at handcrafting per-page layouts pleasing to the eye. That free-form space to generate and develop ideas isn't available in any computer-mediated space, let alone online." (make-side-note "I can see an environment like Adobe Illustrator, combined with a pen pad like a Wacom Intuos, getting you most of the way there. But the responsiveness of ink & paper, combined with how readily it can be used wherever you are, still win out." "side-note")]]
+
+
